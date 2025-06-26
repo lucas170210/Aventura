48b17a186e00ec251a4e4f692158f4d104045039
lucas170210 <lucas.vicente.souza17@escola.pr.gov.br>
2025-06-26 10:50:38 +0000

    26/05/2025

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelectorAll( ' .btn-proximo');
+
+avanca.forEach (button =>{
+   button.addEventlistener('click',function(){
+       const atual = document.querySelector ('ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+   })
+
+})
